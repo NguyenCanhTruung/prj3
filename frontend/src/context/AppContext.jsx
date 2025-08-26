1e40c3a8223fe91f046e8bbcb5c621659f82ae37
NguyenCanhTruung <Trung.nc220121@sis.hust.edu.vn>
2025-08-26 08:58:28 +0700

    create done real data from backend anh call this to frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,7 +1,9 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import { doctors } from '../assets/assets';
+import axios from 'axios';
+import {toast} from 'react-toastify'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
@@ -9,12 +11,31 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
 
     const currencySymbol = '.000đ';
-
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [doctors, setDoctors] = useState([])
 
     const value = {
         doctors,
         currencySymbol
     }
+
+    const getDoctorsData = async () =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success) {
+                setDoctors(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getDoctorsData()
+    },[])
 
     return (
         <AppContext.Provider value={value}>
