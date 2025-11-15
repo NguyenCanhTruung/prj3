4d563dca42e79ec28add25170d767f8436e6e5c3
NguyenCanhTruung <Trung.nc220121@sis.hust.edu.vn>
2025-11-15 09:13:05 +0700

    hoàn thiện trang dashboard cho admin nhằm quản lí bác sĩ bệnh nhân và lịch hẹn

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -13,6 +13,7 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
   const [doctors, setDoctors] = useState([])
   const [appointments, setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false)
   const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   const getAllDoctors = async () => {
@@ -78,6 +79,21 @@ const AdminContextProvider = (props) => {
     }
   }
 
+  const getDashData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } })
+      if (data.success) {
+        setDashData(data.dashData)
+        console.log(data.dashData);
+        
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(data.message)
+    }
+  }
+
   const value = {
     aToken, setAToken,
     backendUrl,
@@ -86,7 +102,8 @@ const AdminContextProvider = (props) => {
     changeAvailability,
     appointments, setAppointments,
     getAllAppointments,
-    cancelAppointment
+    cancelAppointment,
+    dashData, getDashData
   }
 
   return (
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,6 +4,7 @@ import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import JWT from "jsonwebtoken";
 import appointmentModel from "../models/appointmentModel.js";
+import userModel from "../models/userModel.js";
 
 // api for adding doctor
 const addDoctor = async (req, res) => {
@@ -162,4 +163,24 @@ const appointmentCancel = async (req, res) => {
   }
 }
 
-export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel };
+//api to get dashboard data for admon panel
+const adminDashboard = async (req,res)=>{
+  try {
+    const doctors = await doctorModel.find({})
+    const users = await userModel.find({})
+    const appointments = await appointmentModel.find({})
+    const dashData = {
+      doctors: doctors.length,
+      appointments: appointments.length,
+      patients: users.length,
+      latestAppointments: appointments.reverse().slice(0,5) 
+    }
+
+    res.json({success:true,dashData})
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel, adminDashboard };
